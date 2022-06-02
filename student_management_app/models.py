6ce13ec7f9b1ef608383504ae673340e1ab20de7
VincentGichohi <vincentegichohi@gmail.com>
2022-06-02 15:26:37 +0300

    session year model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,3 +1,11 @@
-from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class SessionYearModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
+    objects = models.Manager()
