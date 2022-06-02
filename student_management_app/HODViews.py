3c95abfa4b5ffab17086942bdaae1d497bc06395
VincentGichohi <vincentegichohi@gmail.com>
2022-06-02 15:14:22 +0300

    Save details into respective models

--- student_management_app/HODViews.py
+++ student_management_app/HODViews.py
@@ -90,3 +90,88 @@ def admin_home(request):
         "student_name_list": student_name_list,
     }
     return render(request, "hod_template/home_content.html", context)
+
+
+
+def add_staff(request):
+    return render(request, "hod_template/add_staff_template.html")
+ 
+ 
+def add_staff_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method ")
+        return redirect('add_staff')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+ 
+        try:
+            user = CustomUser.objects.create_user(username=username,
+                                                  password=password,
+                                                  email=email,
+                                                  first_name=first_name,
+                                                  last_name=last_name,
+                                                  user_type=2)
+            user.staffs.address = address
+            user.save()
+            messages.success(request, "Staff Added Successfully!")
+            return redirect('add_staff')
+        except:
+            messages.error(request, "Failed to Add Staff!")
+            return redirect('add_staff')
+ 
+ 
+ 
+def manage_staff(request):
+    staffs = Staffs.objects.all()
+    context = {
+        "staffs": staffs
+    }
+    return render(request, "hod_template/manage_staff_template.html", context)
+ 
+ 
+def edit_staff(request, staff_id):
+    staff = Staffs.objects.get(admin=staff_id)
+ 
+    context = {
+        "staff": staff,
+        "id": staff_id
+    }
+    return render(request, "hod_template/edit_staff_template.html", context)
+ 
+ 
+def edit_staff_save(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        staff_id = request.POST.get('staff_id')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+ 
+        try:
+            # INSERTING into Customuser Model
+            user = CustomUser.objects.get(id=staff_id)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.username = username
+            user.save()
+             
+            # INSERTING into Staff Model
+            staff_model = Staffs.objects.get(admin=staff_id)
+            staff_model.address = address
+            staff_model.save()
+ 
+            messages.success(request, "Staff Updated Successfully.")
+            return redirect('/edit_staff/'+staff_id)
+ 
+        except:
+            messages.error(request, "Failed to Update Staff.")
+            return redirect('/edit_staff/'+staff_id)
