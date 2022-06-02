a075b50577f2da5e535a7ae76d210d6f31332aef
VincentGichohi <vincentegichohi@gmail.com>
2022-06-02 14:33:14 +0300

    Add a staffview and home page

--- student_management_app/StudentViews.py
+++ student_management_app/StudentViews.py
@@ -157,3 +157,51 @@ def student_feedback_save(request):
         except:
             messages.error(request, "Failed to Send Feedback.")
             return redirect('student_feedback')
+
+def student_profile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    student = Students.objects.get(admin=user)
+ 
+    context={
+        "user": user,
+        "student": student
+    }
+    return render(request, 'student_template/student_profile.html', context)
+ 
+ 
+def student_profile_update(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('student_profile')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+ 
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+            if password != None and password != "":
+                customuser.set_password(password)
+            customuser.save()
+ 
+            student = Students.objects.get(admin=customuser.id)
+            student.address = address
+            student.save()
+             
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('student_profile')
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return redirect('student_profile')
+ 
+ 
+def student_view_result(request):
+    student = Students.objects.get(admin=request.user.id)
+    student_result = StudentResult.objects.filter(student_id=student.id)
+    context = {
+        "student_result": student_result,
+    }
+    return render(request, "student_template/student_view_result.html", context)
