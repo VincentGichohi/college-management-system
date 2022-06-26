549f80370de6b3e25f6bde9f02ec6d7522decfe6
VincentGichohi <vincentegichohi@gmail.com>
2022-06-26 19:51:41 +0300

    Add baseusermanager and permissionsmixin

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,9 +1,13 @@
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+ALLOWED_GENDER = [
+    ("MALE", "MALE"),
+    ("FEMALE", "FEMALE")
+]
 class SessionYearModel(models.Model):
     id = models.AutoField(primary_key=True)
     session_start_year = models.DateField()
@@ -12,7 +16,7 @@ class SessionYearModel(models.Model):
 
 # Overriding the Default Django Auth
 # User and adding One More Field (user_type)
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     HOD = '1'
     STAFF = '2'
     STUDENT = '3'
@@ -71,7 +75,7 @@ class Subjects(models.Model):
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
     admin = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
-    gender = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50, choices=ALLOWED_GENDER)
     profile_pic = models.FileField()
     address = models.TextField()
     course_id = models.ForeignKey(Courses, on_delete=models.DO_NOTHING, default=1)
