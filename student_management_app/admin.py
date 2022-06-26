9d17f7294ca15f96c1958b18e1e0e78fcd2fe59e
VincentGichohi <vincentegichohi@gmail.com>
2022-06-26 21:47:45 +0300

    modify an admin file

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -2,10 +2,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, LeaveReportStaff, FeedBackStudent, FeedBackStaffs, NotificationStudent, NotificationStaffs
- 
+from django.contrib.auth import get_user_model
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 # Register your models here.
+
+
+User = get_user_model()
 class UserModel(UserAdmin):
-    pass
+    #The forms to add and change user instances.
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    #The fields to be used in displaying the User Model.
+    #These override the definition on the base UserAdmin
+    #that references specific fields on auth.user
+    list_display = ['email', 'admin']
+    list_filter = ['admin']
+    fieldsets = (
+        (None, {'felds': 'email', 'password'}),
+        ('Personal Info', {'fields': ()}),
+        ()
+    )
  
  
 admin.site.register(CustomUser, UserModel)
