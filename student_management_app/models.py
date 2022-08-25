bf925515d092231b73a01ea48d4fb767443632fd
VincentGichohi <vincentegichohi@gmail.com>
2022-08-25 15:36:35 -0400

    add a subject model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -89,3 +89,14 @@ class Staff(models.Model):
         return self.admin.last_name + " , " + self.admin.first_name
 
 
+class Subject(models.Model):
+    name = models.CharField(max_length=120)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
