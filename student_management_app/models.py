c67b17e1b62469cbd66210b69f39e446e6a312a2
VincentGichohi <vincentegichohi@gmail.com>
2022-08-25 15:15:46 -0400

    add a courses model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -59,3 +59,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class Admin(models.Model):
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
+
+class Courses(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
