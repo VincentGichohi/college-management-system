aa659eb084a5654bc6243f6d7d9d3cf511e18549
VincentGichohi <vincentegichohi@gmail.com>
2022-08-25 16:03:57 -0400

    edit configurations

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -172,6 +172,7 @@ class StudentResult(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
