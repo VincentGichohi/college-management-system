76bc710c9249dcbd65a90d049fc3d253e4ae6c02
VincentGichohi <vincentegichohi@gmail.com>
2022-08-25 15:49:25 -0400

    add a feedback staff model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -140,3 +140,11 @@ class FeedbackStudent(models.Model):
     reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class FeedbackStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
