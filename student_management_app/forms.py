b910b34dd96bf0c294a6744ccf43c9d2f2bc1283
VincentGichohi <vincentegichohi@gmail.com>
2022-08-26 08:35:35 -0400

    add a clean email method

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -1,7 +1,7 @@
 
 from django import forms
 from django.forms.widgets import DateInput, TextInput
-
+from .models import CustomUser
 
 class FormSettings(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -11,7 +11,7 @@ class FormSettings(forms.ModelForm):
             field.field.widget.attrs['class'] = 'form-control'
 
 
-class CustomerForm(FormSettings):
+class CustomUserForm(FormSettings):
     email = forms.EmailField(required=True)
     gender = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')])
     first_name = forms.CharField(required=True)
@@ -22,6 +22,35 @@ class CustomerForm(FormSettings):
         'password': forms.PasswordInput(),
     }
     profile_pic = forms.ImageField()
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserForm, self).__init__(*args, **kwargs)
+
+        if kwargs.get('instance'):
+            instance = kwargs.get('instance').admin.__dict__
+            self.fields['password'].required = False
+            for field in CustomUserForm.Meta.fields:
+                self.fields['field'].initial = instance.get(field)
+            if self.instance.pk is not None:
+                self.fields['password'].widget.attrs['placeholder'] = "Fill this only if you wish to update the " \
+                                                                      "password"
+
+    def clean_email(self, *args, **kwargs):
+        formEmail = self.cleaned_data['email'].lower()
+        if self.instance.pk is None:
+            if CustomUser.objects.filter(email=formEmail).exists():
+                raise forms.ValidationError(
+                    "The given email is already registered."
+                )
+            else:
+                dbEmail = self.Meta.model.objects.get(
+                    id=self.instance.pk).admin.email.lower()
+                if dbEmail != formEmail:
+                    if CustomUser.objects.filter(email=formEmail).eixsts():
+                        raise forms.ValidationError("The given email is already registered")
+
+            return formEmail
+
 
 
 
