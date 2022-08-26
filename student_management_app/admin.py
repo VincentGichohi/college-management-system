0c9fa54effee65a019172bffcd8fca9ef98544a3
VincentGichohi <vincentegichohi@gmail.com>
2022-08-26 09:31:51 -0400

    import models to apps.py

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -1,7 +1,11 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from student_management_app.models import *
 
 
 class UserModel(UserAdmin):
     ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserModel)
