3ddd7346d4357ec80a977983d718ae698c3de8bf
VincentGichohi <vincentegichohi@gmail.com>
2022-08-26 03:50:13 -0400

    add a formsettings form

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -2,3 +2,13 @@
 from django import forms
 from django.forms.widgets import DateInput, TextInput
 
+class FormSettings(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(FormSettings, self).__init__(*args, **kwargs)
+        # Here make some changes such as
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
+
+
+
