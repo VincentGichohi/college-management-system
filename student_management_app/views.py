8c3c1e56105452e6dec98094060c232df5dc51a1
VincentGichohi <vincentegichohi@gmail.com>
2022-08-26 15:07:03 -0400

    import csrf exempt to views

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -4,3 +4,6 @@ import requests
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views.decorators.csrf import csrf_exempt
+
