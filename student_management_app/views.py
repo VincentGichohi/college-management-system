0df23e0fb5b504ff4d842e616247eac21e213aa6
VincentGichohi <vincentegichohi@gmail.com>
2022-08-26 15:22:10 -0400

    add a login page view

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -11,4 +11,12 @@ from .EmailBackend import EmailBackend
 from .models import Session, Attendance, Subject
 
 def login_page(request):
+    if request.user.is_authenticated:
+        if request.user_type == '1':
+            return redirect(reverse("admin_home"))
+        elif request.user_type == '2':
+            return redirect(reverse("staff_home"))
+        else:
+            return redirect(reverse("student_home"))
+    return render(request, 'student_management_app/login.html')
 
