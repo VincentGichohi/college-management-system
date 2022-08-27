8023760729036ed874ca9f265141697d65a51471
VincentGichohi <vincentegichohi@gmail.com>
2022-08-27 07:31:27 -0400

    checking the authenticated person and the user type

--- student_management_app/middleware.py
+++ student_management_app/middleware.py
@@ -1,7 +1,14 @@
-
 from django.utils.deprecation import MiddlewareMixin
 from django.urls import reverse
 from django.shortcuts import redirect
 
+
 class LoginCheckMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
+        modulename = view_func.__module__
+        user = request.user  # Check the current user
+        if user.is_authenticated:
+            if user.user_type == '1':  # Is it the HOD/Admin?
+                if modulename == 'student_management_app.student_views':
+                    return redirect(reverse('admin_home'))
+            elif user.user_type == '2':  # Staff
