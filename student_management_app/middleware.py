9e14b8a6921a75bf275853bc998afd329fcbc05e
VincentGichohi <vincentegichohi@gmail.com>
2022-08-27 07:39:17 -0400

    use an if statement to check the user types and authenticationg

--- student_management_app/middleware.py
+++ student_management_app/middleware.py
@@ -12,3 +12,13 @@ class LoginCheckMiddleware(MiddlewareMixin):
                 if modulename == 'student_management_app.student_views':
                     return redirect(reverse('admin_home'))
             elif user.user_type == '2':  # Staff
+                if modulename == 'student_management_app.student_views' or modulename == 'student_management_app' \
+                                                                                         '.hod_views':
+                    return redirect(reverse('student_home'))
+            else:
+                return redirect(reverse('login_page'))
+        else:
+            if request.path == reverse('login_page') or modulename == 'django.contrib.auth.views' or request.path == reverse('user_login'):  # If
+                pass
+            else:
+                return redirect(reverse('login_page'))
