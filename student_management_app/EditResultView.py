97a3d98ed72c4626448e59f752ad17d2972c9e91
VincentGichohi <vincentegichohi@gmail.com>
2022-08-27 07:50:04 -0400

    add a get method

--- student_management_app/EditResultView.py
+++ student_management_app/EditResultView.py
@@ -4,3 +4,14 @@ from django.contrib import messages
 from .models import Subject, Staff, Student, StudentResult
 from .forms import EditResultForm
 from django.urls import reverse
+
+class EditResultView(View):
+    def get(self, request, *args, **kwargs):
+        resultForm = EditResultForm()
+        staff = get_object_or_404(Staff, admin=request.user)
+        resultForm.fields['subject'].queryset = Subject.objects.filter(staff=staff)
+        context = {
+            'form': resultForm,
+            'page_title': "Edit Student's Result"
+        }
+        return render(request, "staff_template/edit_student_result.html", context)
