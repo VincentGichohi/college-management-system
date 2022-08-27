136e9593b46f5ef7f8bbf4cd4650a3b2f6686703
VincentGichohi <vincentegichohi@gmail.com>
2022-08-27 07:27:11 -0400

    add a loginmiddleware class

--- student_management_app/middleware.py
+++ student_management_app/middleware.py
@@ -2,3 +2,6 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.urls import reverse
 from django.shortcuts import redirect
+
+class LoginCheckMiddleware(MiddlewareMixin):
+    def process_view(self, request, view_func, view_args, view_kwargs):
