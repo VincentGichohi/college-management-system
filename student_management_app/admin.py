b71fb6c9d19930c99cb5ccdfcaf60a6310edf193
VincentGichohi <vincentegichohi@gmail.com>
2022-08-28 22:30:23 +0300

    add form to admin class

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -5,8 +5,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from .forms import UserAdminCreationForm, UserAdminChangeForm
-
+from .forms import *
 class UserModel(UserAdmin):
     ordering = ('email',)
 
@@ -29,8 +28,8 @@ admin.site.unregister(Group)
 
 class UserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
-    form = UserAdminChangeForm
-    add_form = UserAdminCreationForm
+    # form = UserAdminChangeForm
+    add_form = CustomUserForm
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
