9dee32670032bf82a892ee3a66a0ab7a4896a033
VincentGichohi <vincentegichohi@gmail.com>
2022-08-28 22:24:17 +0300

    modify customuser contents

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import UserManager
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
 
 
 class CustomUserManager(UserManager):
