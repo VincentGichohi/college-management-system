92c9b294f7eb8575417a8bc36b55c05d95ebf2b6
VincentGichohi <vincentegichohi@gmail.com>
2022-12-17 23:50:25 +0300

    add auth user model

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -126,4 +126,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'student_management_app.CustomUser'
+AUTH_USER_MODEL = 'student_management_app.CustomUser'
