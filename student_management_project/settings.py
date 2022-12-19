a2799f636f9f11ad1b843923214e6e812ec74db5
VincentGichohi <vincentegichohi@gmail.com>
2022-12-19 23:20:06 +0300

    add a dockerfile

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'student_management_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cms',
+        'PORT': '5432',
+        'HOST': '',
+        'USERNAME': 'postgres',
+        'PASSWORD': '0000'
     }
 }
 
