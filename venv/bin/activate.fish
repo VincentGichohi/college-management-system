2e61cebb46857f0484c0e8c7a88fce335cfa54e2
VincentGichohi <vincentegichohi@gmail.com>
2023-01-08 19:04:28 +0000

    edit virtual environments

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vinny/Documents/Code-2/college-management-system/venv"
+set -gx VIRTUAL_ENV "/home/VincentGichohi/college-management-system/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
